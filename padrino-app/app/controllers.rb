c815a05fcbcf0bc08d332c1ba5ab3ce88cbff79c
Darío Javier Cravero <dario@uxtemple.com>
2014-06-20 01:08:17 +0100

    Implemented WS.

--- padrino-app/app/controllers.rb
+++ padrino-app/app/controllers.rb
@@ -1,5 +1,12 @@
 PadrinoApp::App.controllers  do
   get :index do
-    'Hello world!'
+    render :index
+  end
+
+  websocket :channel do
+    on :ping do |message|
+      send_message(:channel, session['websocket_user'], {pong: true, data: message})
+      broadcast(:channel, {pong: true, data: message, broadcast: true})
+    end
   end
 end
